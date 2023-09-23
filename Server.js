c4c0ebb6256965518e9ad0b60e81cc9df2848d2e
Akshay Neje <akshayneje2823@gmail.com>
2023-09-23 14:17:08 +0530

    dskk

--- Server.js
+++ Server.js
@@ -27,7 +27,7 @@ const app = express();
 
 // setting limit
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true }));
+app.use(bodyParser.urlencoded({  extended: true }));
 
 // to read data in json format
 app.use(express.json());
