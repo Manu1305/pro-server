7dc3849e22ff95160a9dbaa7ee382fc67b819448
Akshay Neje <akshayneje2823@gmail.com>
2023-10-11 18:57:36 +0530

    1 rs payment undo

--- src/controllers/PaymentController.js
+++ src/controllers/PaymentController.js
@@ -18,8 +18,8 @@ const instance = new Razorpay({
 // order creation
 const checkout = async (req, res, next) => {
   const options = {
-    // amount: Number(req.body.amount) * 100,
-    amount: 1*100,
+    amount: Number(req.body.amount) * 100,
+    // amount: 1*100,
 
     currency: "INR",
   };
