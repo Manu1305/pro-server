bdc7ced1ef4b6806e956d54c5e4d236c3dcc2dfa
Certon Admin <it.team@certontech.com>
2023-10-20 18:28:05 +0530

    cors isseue

--- Server.js
+++ Server.js
@@ -34,16 +34,11 @@ app.use(express.json());
 
 // cors policy
 
-const allowedOrigins = ['https://hitecmart.com'];
-
 const corsOptions = {
-  origin: function (origin, callback) {
-    if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: 'https://hitecmart.com', // Replace with the actual origin of your client application
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // If you need to pass cookies or other credentials
+  optionsSuccessStatus: 204, // For preflight requests
 };
 
 app.use(cors(corsOptions));
